c943c77f4504744f2b2914479c993834c80ae36a
lucasr28 <lucasr28@gmail.com>
2014-11-04 00:21:57 -0300

    Correcciones varias en distintas consultas3.

---
+++ www/js/Views/DrogueriasView/DrogueriaNominalView.js
@@ -0,0 +1,104 @@
+var DrogueriaNominalView = (function ($, renderer, BaseView, DrogueriaNominalGeneralView, GoogleMapView) {
+    "use strict";
+
+    var drogueriaNominalView = TabPanelView.extend({
+        tagName: 'div',
+        className: 'panel consulta-detallada',
+
+        attributes: {
+            'id': 'resultadoConsultaNominalDrogueria',
+            'data-nav': 'consultas_nav'
+        },
+
+        codigoDrogueria: null,
+
+        setCodigoDrogueria: function (codigoDrogueria) {
+            this.codigoDrogueria = codigoDrogueria;
+        },
+
+        getCodigoDrogueria: function (codigoDrogueria) {
+            return this.codigoDrogueria;
+        },
+
+        tabs: [
+            {
+                tabName: "General",
+                panelId: "drogueriaGeneral",
+                viewClass: DrogueriaNominalGeneralView,
+                modelClass: DrogueriaNominal
+            },
+            {
+                tabName: "Mapas",
+                panelId: "drogueriaMapas",
+                viewClass: GoogleMapView
+            }
+        ],
+
+        ejecutarConsultaNominalDrogueria: function (codigoDrogueria) {
+            this.codigoDrogueria = codigoDrogueria;
+            var tabGeneral = this.findTab("panelId", "drogueriaGeneral");
+            tabGeneral.view.model.load(this.codigoDrogueria);
+        },
+
+        initialize: function (attributes, options) {
+            options = options || {};
+            options.renderer = renderer;
+            this.setCodigoDrogueria(attributes.codigo);
+            TabPanelView.prototype.initialize.call(this, attributes, options);
+
+            this.findTab("panelId", "drogueriaGeneral").filtroConsulta = _.bind(this.getCodigoDrogueria, this);
+            var mapaDrogueriaTab = this.findTab("panelId", "drogueriaMapas");
+            mapaDrogueriaTab.onViewRenderedHandler = _.bind(this.onGoogleMapViewRendered, this);
+            mapaDrogueriaTab.modelClass = this.getCoordenadasMapaModel();
+        },
+        /**
+        * Devuelve el model asociado a la vista, que se muestra en uno de los tabs.
+        * @param {String} tabName, nombre del tab, a partir de este se obtiene el modelo
+        */
+        getModelOrDefault: function (tabName) {
+            var tab = this.findTab("tabName", tabName);
+            return tab.view.model;
+        }
+    });
+
+    /**
+     * Devuelve una vista por nombre.
+     *  @param {String} , el nombre de la vista, si se quiere poder acceder a la misma mediante nombre. 
+     */
+    drogueriaNominalView.prototype.getViewByName = function (viewName) {
+        var tab = this.findTab("tabName", viewName);
+        return tab.view;
+    };
+
+    drogueriaNominalView.prototype.renderSelectedTab = function (args) {
+        //Si se hizo clic en el tab de mapas, se carga el mapa
+        if (!common.isEmpty(args)) {
+            var selectedTabPanelId = common.trimLeft(args.currentTarget.getAttribute("href"), "#");
+            if (selectedTabPanelId === "drogueriaMapas") {
+                var tabMapa = this.findTab("panelId", selectedTabPanelId);
+                $.ui.loadContent(tabMapa.view.attributes.id, false, false, "pop");
+            }
+        } else {
+            //Cuando se carga el tab panel view por primera vez, después de inicializar el mapa, viene por este laod.
+            TabPanelView.prototype.renderSelectedTab.call(this, args);
+        }
+        $("#" + this.attributes.id).addClass("consultaNominalDrogueria");
+    };
+    /**
+    * Obtiene el modelo de coordenadas a partir del modelo de drogueria nominal.
+    */
+    drogueriaNominalView.prototype.getCoordenadasMapaModel = function () {
+        var tabGeneral = this.findTab("panelId", "drogueriaGeneral");
+        return tabGeneral.view.model.get("coordenadasDeMapa");
+    };
+
+    drogueriaNominalView.prototype.onGoogleMapViewRendered = function () {
+        //Si se hizo clic en el tab de mapas, se carga el mapa de forma diferente, no se muestra dentro del tab
+        if (this.selectedTab.panelId === "drogueriaMapas") {
+            var tabMapa = this.findTab("panelId", "drogueriaMapas");
+            $.ui.loadContent(tabMapa.view.getViewId(), false, false, "pop");
+            return;
+        }
+    };
+    return drogueriaNominalView;
+}(af, AppFrameworkRenderer, BaseView, DrogueriaNominalGeneralView, GoogleMapView));
---
+++ www/js/Views/FarmaciasView/ConsultaFarmaciasView.js
@@ -0,0 +1,92 @@
+var ConsultaFarmaciasView = (function (jquery, $, renderer, BaseView, ListadoFarmaciasView, BusquedaFarmaciaView) {
+    "use strict";
+
+    var consultaFarmaciasView = BaseView.extend({
+        tagName: 'div',
+
+        attributes: {
+            'id': 'consultaFarmacias',
+            'class': 'panel',
+//            'data-title': 'REFAR',
+            'data-nav':"consultas_nav",    
+        },
+
+        template : _.template(
+            '<div class="formGroupHead">Registro Federal de Farmacias. Seleccione una opción.</div>' +
+            '<form>' +
+            '<div class="button-grouped vertical">' +
+            '<a id="submitListadoFarmacias" class="button">Listado de Farmacias</a>' +
+            '<a id="submitBuscarFarmacia" class="button">Buscar Farmacia</a>' +
+            '</div>' +
+            '</form>'
+        ),
+
+        initialize: function(attributes, options) {
+            options = options || {};
+            options.renderer = renderer;
+            BaseView.prototype.initialize.call(this, attributes, options);
+
+            this.initializeModelDataSource();
+        },
+
+        initializeModelDataSource: function () {
+            this.modelDataSource = new ModelDataSource ({view: this});
+            this.modelDataSource.on('dataFetched', this.renderVistaDeDatos, this);
+        },
+
+        /**
+        * Hace el render de la vista que muestra los datos del modelo que se obtuvieron a partir de la consulta
+        * aplicando los filtros.
+        * @param {Object} data, información del modelo obtenida del servicio.
+        */
+        /*    renderVistaDeDatos: function (data) {
+            var ministerioModel = new Ministerio();
+            var ministerioView = MinisterioView.getInstance();
+            ministerioView.setModel({model: ministerioModel});
+            ministerioModel.processData(data);
+        },*/
+
+        ejecutarBuscarFarmacia: function(){
+            var busquedaFarmaciaView = new BusquedaFarmaciaView();
+            busquedaFarmaciaView.render();
+        },
+
+        ejecutarListadoFarmacias: function(){
+            var listadoFarmaciasView = new ListadoFarmaciasView();
+            listadoFarmaciasView.render();
+        },
+
+        render: function(){
+            //            $.ui.addContentDiv("consultaFarmacias", this.template());
+            //            $.ui.loadContent("consultaFarmacias", false, false, "slide");
+
+            /*if ($("#consultaFarmacias").length <= 0){
+                $.ui.addContentDiv("consultaFarmacias", this.template());//div panel + contenido
+            }else
+            {
+                $.ui.updatePanel("consultaFarmacias", this.template());//solo contenido para actualizar
+            }
+            $.ui.loadContent("consultaFarmacias", false, false, "slide");
+            $("#consultaFarmacias").addClass("consulta-detallada"); //agrego esta clase para poder aplicar estilos CSS
+*/
+
+            //this.attachEvents();
+            BaseView.prototype.render.call(this);
+            FastClick.attach($("#submitListadoFarmacias"));
+            FastClick.attach($("#submitBuscarFarmacia"));
+            return this;
+        },
+
+        /**
+         * Usado para bindear eventos a los controles del formulario.
+         * Se ejecuta después del render cuando los controles se encuentran cargados en la página. 
+         */
+        attachEvents: function(){
+            BaseView.prototype.attachEvents.call(this);
+            $("#afui").delegate(this.getViewSelector() + " a#submitListadoFarmacias", "click", _.bind(this.ejecutarListadoFarmacias, this));
+            $("#afui").delegate(this.getViewSelector() + " a#submitBuscarFarmacia", "click", _.bind(this.ejecutarBuscarFarmacia, this));
+        }
+    });
+
+    return consultaFarmaciasView;
+})(jQuery, af, AppFrameworkRenderer, BaseView, ListadoFarmaciasView, BusquedaFarmaciaView);
