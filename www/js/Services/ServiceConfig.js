8061dc9ff9a7abd24f74e7f8d5e6148bdbac7b1f
lucasr28 <lucasr28@gmail.com>
2014-11-15 19:21:38 -0300

    Corrección de inicio de sesión. Agregado de cierre de sesión. Utilización de local storage para persistir los datos.

---
+++ www/js/Services/ServiceConfig.js
@@ -0,0 +1,13 @@
+/**
+ * Configuración general de los servicios 
+ */
+var ServiceConfig = 
+    {
+        baseUrl: "https://sisa.msal.gov.ar/sisa/services/rest",
+        /*baseUrl: "https://qa.sisa.msal.gov.ar/sisaqa/services/rest",*/
+        enableCors: false,//false cuando se haga el build de la aplicación. Sirve para evitar errores con CORS cuando se debuguea la app desde el browser.
+        serviceProvider: 'jquery', /*o custom*/
+        timeout: 60000, /*tiempo de espera ilimitado*/
+        usuario: null,
+        clave: null
+    };
