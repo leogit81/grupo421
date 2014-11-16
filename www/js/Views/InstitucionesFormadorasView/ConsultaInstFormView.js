e6ba1e735fdfbedad305fc8c3995be2e2de19a3a
lucasr28 <lucasr28@gmail.com>
2014-11-16 02:01:09 -0300

    Agregado de estilos para botones, inputs, selects.

---
+++ www/js/Views/InstitucionesFormadorasView/ConsultaInstFormView.js
@@ -0,0 +1,63 @@
+var ConsultaInstFormView = (function (jquery, $, renderer, BaseView, BusquedaInstFormView, ListadoInstFormView) { 
+    "use strict";
+
+    var consultaInstFormView = BaseView.extend({
+        tagName: 'div',
+
+        attributes: {
+            'id': 'consultaInstForm',
+            'class': 'panel',
+            'data-nav':"consultas_nav",
+        },
+
+        template : _.template(
+            '<div class="formGroupHead">Registro Federal de Instituciones Formadoras. Seleccione una opción.</div>' +
+            '<form>' +
+            '<div>' +
+            '<a id="submitListadoInstForm" class="button">Listado de Instituciones Formadoras</a>' +
+            '<br>' +
+            '<a id="submitBuscarInstForm" class="button">Buscar Institución Formadora</a>' +
+            '</div>' +
+            '</form>'
+        ),
+
+        initialize: function(attributes, options) {
+            options = options || {};
+            options.renderer = renderer;
+            BaseView.prototype.initialize.call(this, attributes, options);
+
+            this.initializeModelDataSource();
+        },
+
+        initializeModelDataSource: function () {
+            this.modelDataSource = new ModelDataSource ({view: this});
+            this.modelDataSource.on('dataFetched', this.renderVistaDeDatos, this);
+        },
+
+        ejecutarBuscarInstForm: function(){
+            var busquedaInstFormView = new BusquedaInstFormView();
+            busquedaInstFormView.render();
+        },
+
+        ejecutarListadoInstForm: function(){
+            var listadoInstFormView = new ListadoInstFormView();
+            listadoInstFormView.render();
+        },
+
+        render: function(){
+            BaseView.prototype.render.call(this);
+            return this;
+        },
+
+        /**
+         * Usado para bindear eventos a los controles del formulario.
+         * Se ejecuta después del render cuando los controles se encuentran cargados en la página. 
+         */
+        attachEvents: function(){
+            BaseView.prototype.attachEvents.call(this);
+            $("#afui").delegate(this.getViewSelector() + " a#submitListadoInstForm", "click", _.bind(this.ejecutarListadoInstForm, this));
+            $("#afui").delegate(this.getViewSelector() + " a#submitBuscarInstForm", "click", _.bind(this.ejecutarBuscarInstForm, this));
+        }
+    });
+    return consultaInstFormView;
+})(jQuery, af, AppFrameworkRenderer, BaseView, BusquedaInstFormView, ListadoInstFormView);
