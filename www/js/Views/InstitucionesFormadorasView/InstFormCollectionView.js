5d7d77ccc55b8683adaac87373ae740fb8149538
lucasr28 <lucasr28@gmail.com>
2014-11-16 21:58:49 -0300

    corrección provincia de intituciones formadoras collection view

---
+++ www/js/Views/InstitucionesFormadorasView/InstFormCollectionView.js
@@ -0,0 +1,30 @@
+var InstFormCollectionView = (function ($, common, Backbone, _, renderer, BaseView, InstFormNominal, InstFormNominalView) {
+    "use strict";
+
+    var instFormCollectionView = BaseCollectionView.extend({
+        tagName: 'div',
+        className: 'panel consulta-detallada',
+
+        attributes: {
+            'id': 'resultadoConsultaGeneralInstForm',
+            'data-nav': "consultas_nav"
+        },
+        
+        itemTemplateString: "<li><a><%=nombre%></br><span class='codigoInstForm'> <%=codigoSISA%> </span> </li>",
+//- <% if (provincia) { %><%=provincia%></a><% } %>
+        busquedaNominalInstForm: function (eventData) {
+            var codigoInstForm = this.getCodigoInstFormFromSelectedItem(eventData.currentTarget.outerHTML),
+                instFormNominalView = new InstFormNominalView({codigo: codigoInstForm});
+            instFormNominalView.loadDefaultView();
+        },
+
+        getCodigoInstFormFromSelectedItem: function (selectedItem) {
+            return common.trim($(selectedItem).find("span.codigoInstForm").html());
+        }
+    });
+
+    instFormCollectionView.prototype.busquedaNominalItem = function (eventData) {
+        this.busquedaNominalInstForm(eventData);
+    };
+    return instFormCollectionView;
+}(af, common, Backbone, _, AppFrameworkRenderer, BaseView, InstFormNominal, InstFormNominalView));
