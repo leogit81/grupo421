125396d72f49f321bada3bfdbc03e64fb68af837
Cropp Utn Grupo421 <cropputn@gmail.com>
2014-11-23 00:42:26 -0300

    se modifica la vista de participaciones para que muestre un mensaje cuando no hay datso

--- www/js/Views/Establecimiento/ParticipacionesView.js
+++ www/js/Views/Establecimiento/ParticipacionesView.js
@@ -14,23 +14,23 @@ var ParticipacionesView = (function (_, BaseView) {
                 "<% if (redEstablecimientosCCC=='SI') { %><div><label>Red de establecimientos de cardiopatías congénitas</label></div><% } %>" +
                 "<% if (sistemaNacionalSangre=='SI') { %><div><label>Sistema Nacional de Sangre</label></div><% } %>" +
                 "<% if (registroSIVILE=='SI') { %><div><label>Unidad centinela registro SIVILE</label></div><% } %>"
-        )
+        ),
+        
+        render: function () {
+            BaseView.prototype.render.call(this);
+            var cuentaDeSI = 0;
+            
+            _.each(this.model.attributes, function (value, key, list) {
+                if (!common.isEmpty(list[key]) && list[key].toUpperCase() === "SI") {
+                    cuentaDeSI++;
+                }
+            });
+            
+            if (cuentaDeSI <= 0) {
+                this.$el.append("No se encuentra información disponible de participaciones.");
+            }
+        }
     });
     
     return participacionesView;
 }(_, BaseView));
-
-
-
-//BACK UP TEMPLATE PREVIO
-//"<div><h2>PARTICIPACIONES</h2></div>" +
-//                "<div><label>Registro de hospital público de gestión descentralizada (HPGD)</label><span><%=registroHPGD%></span></div>" +
-//                "<div><label>Programa Remediar</label><span><%=programaRemediar%></span></div>" +
-//                "<div><label>Plan Nacer</label><span><%=planNacer%></span></div>" +
-//                "<div><label>Programa Médicos Comunitarios</label><span><%=programaMedicosComunitarios%></span></div>" +
-//                "<div><label>Sistema Nacional de Vigilancia de la Salud</label><span><%=sistemaNacionalVigilanciaSalud%></span></div>" +
-//                "<div><label>Red Directores de Hospitales</label><span><%=redDirectoresHospitales%></span></div>" +
-//                "<div><label>Red vacunatorios NOMIVAC</label><span><%=redNOMIVAC%></span></div>" +
-//                "<div><label>Red de establecimientos de cardiopatías congénitas</label><span><%=redEstablecimientosCCC%></span></div>" +
-//                "<div><label>Sistema Nacional de Sangre</label><span><%=sistemaNacionalSangre%></span></div>" +
-//                "<div><label>Unidad centinela registro SIVILE</label><span><%=registroSIVILE%></span></div>"
