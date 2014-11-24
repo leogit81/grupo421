2a689b8c80fa2a24cd786cfcaa29aa11f8134ae3
leogit81 <leojava2007@gmail.com>
2014-11-23 23:59:06 -0300

    se agregan estilos a la vista de contacto

--- www/js/Views/ContactoView.js
+++ www/js/Views/ContactoView.js
@@ -19,11 +19,11 @@ var ContactoView = (function ($, BaseView, renderer) {
         
         template: _.template(
             '<div class="contacto">'+
-            '<span class="nombreMinisterioSalud">Ministerio de Salud de la Nación</span>' +
-            '<label>Tel</label><span class="telefonoMinisterioSalud">54-11-4379-9000</span>' +
-            '<label>Dirección</label><span class="direccionMinisterioSalud">Av. 9 de Julio 1925 (C1073ABA) - Buenos Aires - República Argentina</span>' +
-            '<label>Soporte técnico y asistencia</label>' +
-            '<span>soporte@sisa.msal.gov.ar</span></div>')
+            '<div><span class="nombreMinisterioSalud">Ministerio de Salud de la Nación</span></div>' +
+            '<div class="row"><label>Teléfono</label></div><div><span class="telefonoMinisterioSalud">54-11-4379-9000</span></div>' +
+            '<div class="row"><label>Dirección</label></div><div><span class="direccionMinisterioSalud">Av. 9 de Julio 1925 (C1073ABA) - Buenos Aires - República Argentina</span></div>' +
+            '<div class="row"><label>Soporte técnico y asistencia</label></div>' +
+            '<div><a href="mailto:soporte@sisa.msal.gov.ar">soporte@sisa.msal.gov.ar</a></div></div>')
     });
     
     return contactoView;
