78e7e6e5ce4c00c779c0d6078e43c48930ac2188
leogit81 <leojava2007@gmail.com>
2014-11-25 02:34:15 -0300

    se cambie el mensaje de datos no disponibles para el tab de prestaciones

--- www/js/Views/Establecimiento/PrestacionCollectionView.js
+++ www/js/Views/Establecimiento/PrestacionCollectionView.js
@@ -5,6 +5,7 @@ var PrestacionCollectionView = (function ($, common, _, renderer, BaseCollection
         tagName: 'div',
         className: 'panel consulta-detallada',
         
+        templateSinDatos: "<span class='coleccionSinDatos'>No hay datos disponibles de prestaciones.</span>",
         attributes: {
             'id': 'resultadoConsultaNominalPrestacionesEstablecimiento',
             'data-nav': "consultas_nav"
